1303b5de2f3ca78dcde7e0dda7fbb6825033b78e
fuadajip <fuadajipratomo@gmail.com>
2018-01-22 20:38:36 +0700

    Finish the todo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+const defaultState = { checked: false };
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'TOGGLE':
+            return { ...state, checked: !state.checked };
+        default:
+            break;
+    }
+    return state;
+};
+
+const store = createStore(reducer);
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {};
+    }
+
+    componentWillMount(){
+        store.subscribe(() => this.setState(store.getState()));
+    }
+
     render() {
+        const onClick = () => store.dispatch({ type : 'TOGGLE' });
+
         return (
-            <h1> Hello, World! </h1>
+           <div>
+                <h1>Todo</h1>
+                <div>
+                    React Redux
+                    <input type="checkbox" checked={!!this.state.checked} onClick={onClick}/>
+                </div>
+                {
+                    this.state.checked ? (<h2>Done</h2>) : null
+                }
+           </div>
         );
     }
 }
 
 ReactDOM.render(
-    (
-    <App />
-), document.getElementById('root'));
+    ( 
+        <App />
+    ),
+document.getElementById('root'));
